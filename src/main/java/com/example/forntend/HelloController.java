508922782fb4208bf648b63a9eaf8780c8c32b27
rang612 <tranhuonggiang6122003@gmail.com>
2023-12-20 01:37:43 +0700

    frontend

--- src/main/java/com/example/forntend/HelloController.java
+++ src/main/java/com/example/forntend/HelloController.java
@@ -1,14 +1,68 @@
 package com.example.forntend;
 
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ImageView Exit;
+
+    @FXML
+    private Label Menu;
+
+    @FXML
+    private Label MenuClose;
+
+    @FXML
+    private AnchorPane slider;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Exit.setOnMouseClicked(event -> {
+            System.exit(0);
+        });
+        slider.setTranslateX(-176);
+        Menu.setOnMouseClicked(event -> {
+            TranslateTransition slide = new TranslateTransition();
+            slide.setDuration(Duration.seconds(0.4));
+            slide.setNode(slider);
+
+            slide.setToX(0);
+            slide.play();
+
+            slider.setTranslateX(-176);
+
+            slide.setOnFinished((ActionEvent e)-> {
+                Menu.setVisible(false);
+                MenuClose.setVisible(true);
+            });
+        });
+
+        MenuClose.setOnMouseClicked(event -> {
+            TranslateTransition slide = new TranslateTransition();
+            slide.setDuration(Duration.seconds(0.4));
+            slide.setNode(slider);
+
+            slide.setToX(-176);
+            slide.play();
+
+            slider.setTranslateX(0);
+
+            slide.setOnFinished((ActionEvent e)-> {
+                Menu.setVisible(true);
+                MenuClose.setVisible(false);
+            });
+        });
     }
 }
